027a0713b59bfdfe9d5dd850043dfe589f1b03e8
mjeammet <marie.jeammet@gmail.com>
2021-09-29 16:13:19 +0200

    begining dynamic

--- bruteforce.py
+++ bruteforce.py
@@ -16,21 +16,21 @@ with open(file_path, mode ='r') as file:
     for line in csvFile:
         # print(line)
         action = dict(line)
-        price = float(action["price"])
-        gain = (price * float(action["profit"])/100)
-        if price <= 0: 
+        price = float(action["price"])        
+        if price <= 0:
             # print(action)
             next
         else:
-            action["gain"] = gain
+            action["gain"] = (price * float(action["profit"])/100)
+            action["price"] = float(action['price'])
             data.append(action)
             # use tuple (name, gain) ? 
 
 ### BRUTEFORCING MY WAY OUT OF THIS
 # 2 ** n AKA the worst thing ever
 
-def to_buy_or_not_to_buy(index_of_action, money_spent, total_gain):
-    
+def maximize_profit(index_of_action, remaining_money, total_gain):
+    """Maximize profit recursively"""
     if index_of_action == len(data):
         # reached last action
         return total_gain, []
@@ -38,23 +38,23 @@ def to_buy_or_not_to_buy(index_of_action, money_spent, total_gain):
         action = data[index_of_action]
         # print(f"Questioning whether to buy {action}")
         # say i don't buy
-        gain1, list1 = to_buy_or_not_to_buy(index_of_action+1, money_spent, total_gain)
+        notbought_profit, notbought_list = maximize_profit(index_of_action+1, remaining_money, total_gain)
 
         # say i buy this action
-        if money_spent+float(action["price"]) < MAX_SPENT:
-            gain2, list2 = to_buy_or_not_to_buy(index_of_action+1, money_spent+float(action["price"]), total_gain+float(action["gain"]))
+        if remaining_money > action["price"]:
+            bought_profit, bought_list = maximize_profit(index_of_action+1, remaining_money-action["price"], total_gain+float(action["gain"]))
         else:
             # allowed by the fact actions are sorted by price
             # if THIS action is too pricey, the following won't be cheaper and we'll reach end of line without buying anything
             # print("Cut short")
             return total_gain, []
 
-        if gain2 > gain1:
-            list2.append(index_of_action)
-            return gain2, list2
+        if bought_profit > notbought_profit:
+            bought_list.append(index_of_action)
+            return bought_profit, bought_list
         else:
-            return gain1, list1
-    
+            return notbought_profit, notbought_list
+
 def print_results(result):
     optimized_achats = result[1]
     optimized_profit = result[0]
@@ -70,11 +70,11 @@ def print_results(result):
         f"\nTotal cost: {total_cost}\n",
         f"Profit: {optimized_profit}")
 
-def optimize_choices(rep = 10):
+def maximize_profit_time(rep = 10):
     total_elapsed = datetime.timedelta(0)
     for i in range(rep):
         start = datetime.datetime.now()
-        optimized_achats = to_buy_or_not_to_buy(0, 0, 0)
+        optimized_achats = maximize_profit(0, 500, 0)
         end = datetime.datetime.now()
         elapsed = end - start
         total_elapsed += elapsed
@@ -83,10 +83,12 @@ def optimize_choices(rep = 10):
     print(f"Number of runs : {rep}\tAverage of {total_elapsed / rep} seconds per run.")
     print_results(optimized_achats)
 
-data = data[:21]
-# 19 actions, max 500 ~ 0.67s avec la liste descendante et remontante (20a )
-# 19 actions, max 500 ~ 0.59s avec la liste remontante seulement (20|500 = 1.19 s, 100|50 = 0.71s)
-# 19 actions, max 500 ~ 0.54s avec la liste triée et le return des gens sans le sou (20|500 = 1.12s, 21|500 = 1.94s, ~3.7.10^274 années)
-data = sorted(data,key= lambda x:x["price"])
-optimize_choices(rep=1)
 
+data = data[:19]
+# print(len(data))
+# 19|500 ~ 0.67s avec la liste descendante et remontante (20a )
+# 19|500 ~ 0.59s avec la liste remontante seulement (20|500 = 1.19 s, 100|50 = 0.71s)
+# 19|500 ~ 0.54s avec la liste triée et le return des gens sans le sou (20|500 = 1.12s, 21|500 = 1.94s, ~3.7.10^274 années)
+# 19|500 ~ 0.41s si on fait money restante - price (au lieu de money depensée + price < 500) et convertir les prix en float direct
+data = sorted(data,key= lambda x:x["price"])
+maximize_profit_time(rep=1)
--- optimized.py
+++ optimized.py
@@ -1,30 +1,33 @@
 import csv
 import math
 # from collections import OrderedDict
-MAX_PER_ACTION = 500
+MAX_SPENT = 50000 # in cents
 
-file_path = "./test_datasets/dataset2_Python+P7.csv"
-data = []
-# opening the CSV file
-with open(file_path, mode ='r') as file:
-   
-    # reading the CSV file
-    csvFile = csv.DictReader(file)
-    # simply csv.reader for a list of list
- 
-    # displaying the contents of the CSV file
-    for line in csvFile:
-        # print(line)
-        action = dict(line)
-        price = float(action["price"])
-        gain = (price * float(action["profit"])/100)
-        if price <= 0 or gain <1 : 
-            next
-        else:
-            action["gain"] = gain
-            action["profitability"] = float(action["profit"])/price
-            data.append(action)
-            # use tuple (name, gain) ? 
+file_path = "./test_datasets/dataset1_Python+P7.csv"
+
+def load_csv(file_path):
+    """Opens a csv file"""
+    data = []
+    with open(file_path, mode ='r') as file:
+    
+        # reading the CSV file
+        csvFile = csv.DictReader(file)
+        # simply csv.reader for a list of list
+    
+        # displaying the contents of the CSV file
+        for line in csvFile:
+            # print(line)
+            action = dict(line)
+            price = float(action["price"])
+            if price <= 0:
+                next
+            else:
+                action["price"] = int(float(action["price"])*100)
+                action["gain"] = (price * float(action["profit"])/100)
+                action["profitability"] = float(action["profit"])/price # ? 
+                data.append(action)
+                # use tuple (name, gain) ?
+    return data
 
 def print_results(optimized_achats):
     total_profit = 0
@@ -32,7 +35,7 @@ def print_results(optimized_achats):
     print("Bought:")
     for action in optimized_achats:
         # action = data[action_num]
-        price = float(action["price"])
+        price = action["price"]/100
         print(f"{action['name']} ({price}€)")
         total_cost += price
         total_profit += float(action["gain"])
@@ -41,21 +44,44 @@ def print_results(optimized_achats):
         f"\nTotal cost: {total_cost}\n",
         f"Profit: {total_profit}")
 
-test = sorted(data,key= lambda x:x["profitability"], reverse=True)
-# print(test)
+def maximize_profit_hs(data):
+    remaining_money = MAX_SPENT
+    actions_bought = []
+    for action in data:
+        # print(f"${action}")
+        price = float(action["price"])
+        if remaining_money > price:
+            remaining_money -= price
+            actions_bought.append(action)
+        else:
+            # print(f"Buying ${action['name']} for ${price} (gain = ${action['gain']}) (total money spent: ${money_spent})")
+            # print(money_spent)
+            next
+    return actions_bought
 
-money_spent = 0.0
-actions_bought = []
-for action in test:
-    print(f"${action}")
-    price = float(action["price"])
-    if money_spent + price > MAX_PER_ACTION:
-        # print(money_spent)
-        next
-    else: 
-        money_spent += price
-        actions_bought.append(action)
-        # print(f"Buying ${action['name']} for ${price} (gain = ${action['gain']}) (total money spent: ${money_spent})")
+def maximize_profit(data):
+    crazy_table = [[0]*len(data)]*len(data)
+    crazy_table = []
+    remaining_money = 20
+    
+    # for action_index in data:
+        # crazy_table.append([1,0])
+    return crazy_table
 
-print_results(actions_bought)
+# def maximize_profit(data):
+#     table_res = {}
+
+#     while table_res[0] == None:
+#         pass
+
+#     for action_index in len(data):
+#         if table_res[action_index] == None:
+
+if __name__ == '__main__':
+    data = load_csv(file_path)
+    data = data[:3]
+    sorted_data = sorted(data,key= lambda x:x["gain"], reverse=True)
+    results = maximize_profit(sorted_data)
+    print(results)
+    # print_results(results)
     
