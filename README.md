3498576b77b9f2db9790a65423000fb45d12b1ed
mjeammet <marie.jeammet@gmail.com>
2021-10-04 18:21:09 +0200

    investigation greedy algo

---
+++ README.md
@@ -0,0 +1,3 @@
+** Bruteforce.py
+
+Explores all possible combinations of actions across the dataset. Complexity 0(2^n)
--- optimized.py
+++ optimized.py
@@ -1,10 +1,10 @@
 import csv
-import math
+import datetime
 
 # 0-1 Knapsack problem 
 # https://simplecodehints.com/blog/knapsack-problem-dynamic-programming-algorithm/
 
-def load_csv(file_path):
+def load_csv(file_path, max_invested=500):
     """Opens a csv file"""
     data = []
     with open(file_path, mode ='r') as file:
@@ -17,19 +17,16 @@ def load_csv(file_path):
         for line in csvFile:
             # print(line)
             action = dict(line)
-            price = int(float(action["price"])*100)
-            profit = float(action["profit"])
-            if price <= 0 or profit < 0:
-                next
-            else:
-                action["price"] = price
-                action["gain"] = price * profit/100
-                # action["profitability"] = float(action["profit"])/price # ? 
+            if float(action["price"]) > 0 and float(action["profit"]) > 0:
+                action["price"] = int(float(action["price"])*100)
+                action["gain"] = action["price"] * float(action["profit"])/100
+                action["ror"] = action["gain"]/action["price"]
                 data.append(action)
-                # use tuple (name, gain) ?
     return data
 
-def make_profit_matrix(data, max_spent):
+def make_profit_matrix(data, money_thresholds):
+    min_price = money_thresholds[0]
+    max_spent = money_thresholds[1]
     data.insert(0, {"name":"action_0", "price":0, "gain":0})
     profit_matrix = []
 
@@ -38,17 +35,19 @@ def make_profit_matrix(data, max_spent):
         # if data_index%100 == 0:
         #       # To track huge datasets
         #     print(f"Matrix filed for action number {data_index}")
-        for remaining_money in range(max_spent+1):
+        for remaining_money in range(min_price-1,max_spent+1):
             price = data[action_index]["price"]
             # print(f"Comparing {remaining_money} and {price}")
             if remaining_money < price:
                 # If we don't have no money, we can't buy
                 profit_matrix[action_index][remaining_money] = profit_matrix[action_index-1][remaining_money]
                 # print(profit_matrix[action_index-1][remaining_money])
+
+                # if we can't buy this one, we won't be able to buy the following
+                # so we can copy the whole remaining line
             else:
                 # If we do have the money, we have a choice to make. What is more profitable between us buying and not buying this action ?  
-                gain = data[action_index]["gain"]
-                option_bought = profit_matrix[action_index-1][remaining_money-price] + gain
+                option_bought = profit_matrix[action_index-1][remaining_money-price] + data[action_index]["gain"]
                 option_notbought = profit_matrix[action_index-1][remaining_money]
                 profit_matrix[action_index][remaining_money] = max(option_bought, option_notbought)
         
@@ -62,6 +61,7 @@ def make_profit_matrix(data, max_spent):
 # * j: maximum weight of the knapsack
 # */
 def recursive_knap(profit_matrix, data, action_index, remaining_money, list_of_bought_actions = []):
+    """Browse profit matrix from maximum gain to first action and/or 0 remaining money."""
     # print(profit_matrix[action_index][remaining_money])
     # print(f"Checking {action_index},{remaining_money} (having already bought {list_of_bought_actions})")
 
@@ -79,6 +79,7 @@ def recursive_knap(profit_matrix, data, action_index, remaining_money, list_of_b
             return recursive_knap(profit_matrix, data, action_index-1, remaining_money, list_of_bought_actions)
 
 def print_results(data, actions_list, sep="\t"):
+    """Show results Sienna's style"""
     optimized_achats = actions_list
     total_cost = 0
     optimized_profit = 0
@@ -87,7 +88,8 @@ def print_results(data, actions_list, sep="\t"):
         action = data[action_index]
         price_in_eur = action["price"]/100
         gain = action["gain"]/100
-        print(f"{action['name']}{sep}{price_in_eur}{sep}{action['profit']}{sep}{gain}")
+        ror = action["ror"]
+        print(f"{action['name']}{sep}{price_in_eur}{sep}{action['profit']}{sep}{gain}{sep}{ror}")
         total_cost += price_in_eur
         optimized_profit += gain
 
@@ -95,34 +97,45 @@ def print_results(data, actions_list, sep="\t"):
         f"\nTotal cost: {round(total_cost,2)}\n",
         f"Profit: {round(optimized_profit,2)}")
 
-
 def main():
-    # file_path = "./test_datasets/test_dataset.csv"
-    file_path = "./test_datasets/dataset2_Python+P7.csv"
-    data = load_csv(file_path)
-    # data = [
-    #     {"name":"action1", "price":2, "gain": 2},
-    #     {"name":"action2", "price":5, "gain": 5},
-    #     {"name":"action3", "price":8, "gain": 33},
-    #     {"name":"action4", "price":2, "gain": 27},
-    # ]
+    MAX_SPENT = 50000 # in cents
 
-    ### Subsetting data
-    # data = data[:10]
-    # [print(action) for action in data]
-    MAX_SPENT = 5000 # in cents
-    data = [action for action in data if action["price"] <= MAX_SPENT]
-    data = sorted(data,key= lambda x:x["price"])
+    fake_data = [
+    {"name":"action1", "price":3, "profit": 0.1, "gain": 2},
+    {"name":"action2", "price":5, "profit": 0.1, "gain": 5},
+    {"name":"action3", "price":8, "profit": 0.1, "gain": 33},
+    {"name":"action4", "price":2, "profit": 0.1, "gain": 27},
+    {"name":"action4", "price":1, "profit": 0.1, "gain": 1},
+    ]
 
-    # Generate the matrix M(0...n, 0...W)
-    profit_matrix = make_profit_matrix(data, MAX_SPENT)
-    # [print(line_num, profit_matrix[line_num]) for line_num in range(len(profit_matrix))]
+    # data = fake_data
+    file_path = "./test_datasets/test_dataset.csv"
+    # file_path = "./test_datasets/dataset1_Python+P7.csv"
+    data = load_csv(file_path, MAX_SPENT)
+    data = [action for action in data if action["price"] > 0 and action["price"] <= MAX_SPENT]
+    if data != []:
+        start = datetime.datetime.now()
+        min_price = min(data, key= lambda x:x["price"])["price"]
 
-    # From maximum gain, recursively browse the matrix towards 0,0
-    results = recursive_knap(profit_matrix, data, len(data)-1, MAX_SPENT)
-    print_results(data, results)
+        # data = data[:10]
+        # [print(action) for action in data]
 
-    # Tester 15 - 2300 parce que ça avait l'air de bugger pour un index out of range ?
+
+        data = sorted(data, key= lambda x:x["ror"], reverse=True)
+        # [print(action) for action in data[:10]]
+        # money_post_greed = MAX_SPENT - greedily_bought["price"]
+
+        # Generate the matrix M(0...n, 0...W)
+        profit_matrix = make_profit_matrix(data, [min_price, MAX_SPENT])
+        print(f"{datetime.datetime.now() - start}")
+        # [print(line_num, profit_matrix[line_num]) for line_num in range(len(profit_matrix))]
+
+        # From maximum gain, recursively browse the matrix towards 0,0
+        results = recursive_knap(profit_matrix, data, len(data)-1, MAX_SPENT)
+        print(f"{datetime.datetime.now() - start}")
+        print_results(data, results)        
+    else:
+        print(f"Input is empty.")
 
 if __name__ == '__main__':
     main()
