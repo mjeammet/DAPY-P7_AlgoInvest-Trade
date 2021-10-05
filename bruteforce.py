19e8a32bdd42533079747dc4a781de9770ba7da4
mjeammet <marie.jeammet@gmail.com>
2021-10-05 17:39:00 +0200

    added greedy and cleaned up

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,7 @@
 import csv
 import datetime
 
-def load_file(path):
+def load_file(file_path):
     data = []
 
     ### READING CSV FILE
@@ -9,7 +9,6 @@ def load_file(path):
     
         # reading the CSV file
         csvFile = csv.DictReader(file)
-        # simply csv.reader for a list of list
     
         # displaying the contents of the CSV file
         for line in csvFile:
@@ -26,42 +25,41 @@ def load_file(path):
                 # use tuple (name, gain) ? 
     return data
 
-### BRUTEFORCING MY WAY OUT OF THIS
-# 2 ** n AKA the worst thing ever
 
-def maximize_profit(index_of_action, remaining_money, total_gain):
+def recursive_maximize_profit(data, index_of_action, remaining_money, total_gain, timed=True):
     """Maximize profit recursively"""
     if index_of_action == len(data):
         # reached last action
         return total_gain, []
+
+    action = data[index_of_action]
+    # print(f"Questioning whether to buy {action}")
+    # say i don't buy
+    notbought_profit, notbought_list = recursive_maximize_profit(data, index_of_action+1, remaining_money, total_gain)
+
+    # say i buy this action
+    if remaining_money >= action["price"]:
+        bought_profit, bought_list = recursive_maximize_profit(data, index_of_action+1, remaining_money-action["price"], total_gain+float(action["gain"]))
     else:
-        action = data[index_of_action]
-        # print(f"Questioning whether to buy {action}")
-        # say i don't buy
-        notbought_profit, notbought_list = maximize_profit(index_of_action+1, remaining_money, total_gain)
+        # allowed by the fact actions are sorted by price
+        # if THIS action is too pricey, the following won't be cheaper and we'll reach end of line without buying anything
+        # print("Cut short")
+        return total_gain, []
 
-        # say i buy this action
-        if remaining_money >= action["price"]:
-            bought_profit, bought_list = maximize_profit(index_of_action+1, remaining_money-action["price"], total_gain+float(action["gain"]))
-        else:
-            # allowed by the fact actions are sorted by price
-            # if THIS action is too pricey, the following won't be cheaper and we'll reach end of line without buying anything
-            # print("Cut short")
-            return total_gain, []
+    if bought_profit > notbought_profit:
+        bought_list.append(action)
+        return bought_profit, bought_list
+    else:
+        return notbought_profit, notbought_list
 
-        if bought_profit > notbought_profit:
-            bought_list.append(index_of_action)
-            return bought_profit, bought_list
-        else:
-            return notbought_profit, notbought_list
 
-def print_results(result):
-    optimized_achats = result[1]
-    optimized_profit = result[0]
+def print_results(list_of_actions):
+    list_of_bought_actions = list_of_actions[1]
+    optimized_profit = list_of_actions[0]
     total_cost = 0
     print("Bought:")
-    for action_num in optimized_achats:
-        action = data[action_num]
+    for action in list_of_bought_actions:
+        # action = data[action_num]
         price_in_eur = float(action["price"])
         print(f"{action['name']} ({price_in_eur}€)")
         total_cost += price_in_eur
@@ -70,26 +68,30 @@ def print_results(result):
         f"\nTotal cost: {total_cost}\n",
         f"Profit: {optimized_profit}")
 
-def maximize_profit_timed(max_spent, rep = 1):
-    total_elapsed = datetime.timedelta(0)
-    for i in range(rep):
-        start = datetime.datetime.now()
-        optimized_achats = maximize_profit(0, max_spent, 0)
-        end = datetime.datetime.now()
-        elapsed = end - start
-        total_elapsed += elapsed
-        # print(f"{elapsed} seconds elapsed.") # ~ 800 000 combinaisons per second
-    
-    print(f"Number of runs : {rep}\tAverage of {total_elapsed / rep} seconds per run.")
-    print_results(optimized_achats)
 
-file_path = "./test_datasets/test_dataset.csv"
-data = load_file(file_path)
-# data = data[:10]
-# [print(action) for action in data]
-# 19|500 ~ 0.67s avec la liste descendante et remontante (20a )
-# 19|500 ~ 0.59s avec la liste remontante seulement (20|500 = 1.19 s, 100|50 = 0.71s)
-# 19|500 ~ 0.54s avec la liste triée et le return des gens sans le sou (20|500 = 1.12s, 21|500 = 1.94s, ~3.7.10^274 années)
-# 19|500 ~ 0.41s si on fait money restante - price (au lieu de money depensée + price < 500) et convertir les prix en float direct
-data = sorted(data,key= lambda x:x["price"])
-maximize_profit_timed(500)
+def bruteforce_algo(data, max_spent, timed=True):
+    start_time = datetime.datetime.now()
+
+    selected_actions = recursive_maximize_profit(data, 0, max_spent, 0)
+    
+    if timed:
+        print(f"Ran in {datetime.datetime.now()- start_time}s.")
+    return selected_actions
+
+
+def main():
+    MAX_INVESTED = 500
+    file_path = "./test_datasets/test_dataset.csv"
+    data = load_file(file_path)
+    # data = data[:10]
+    # [print(action) for action in data]
+    # 19|500 ~ 0.67s avec la liste descendante et remontante
+    # 19|500 ~ 0.59s avec la liste remontante seulement (20|500 = 1.19 s, 100|50 = 0.71s)
+    # 19|500 ~ 0.54s avec la liste triée et le return des gens sans le sou (20|500 = 1.12s, 21|500 = 1.94s, ~3.7.10^274 années)
+    # 19|500 ~ 0.41s si on fait money restante - price (au lieu de money depensée + price < 500) et convertir les prix en float direct
+    data = sorted(data,key= lambda x:x["price"])
+    selected_actions = bruteforce_algo(data, MAX_INVESTED)
+    print_results(selected_actions)
+
+if __name__ == '__main__':
+    main()
--- optimized.py
+++ optimized.py
@@ -1,11 +1,10 @@
 import csv
 import datetime
 
-# 0-1 Knapsack problem 
-# https://simplecodehints.com/blog/knapsack-problem-dynamic-programming-algorithm/
 
-def load_csv(file_path, max_invested=500):
+def load_csv(file_path):
     """Opens a csv file"""
+
     data = []
     with open(file_path, mode ='r') as file:
     
@@ -20,31 +19,42 @@ def load_csv(file_path, max_invested=500):
             if float(action["price"]) > 0 and float(action["profit"]) > 0:
                 action["price"] = int(float(action["price"])*100)
                 action["gain"] = action["price"] * float(action["profit"])/100
-                action["ror"] = action["gain"]/action["price"]
+                # action["ror"] = action["gain"]/action["price"]
                 data.append(action)
     return data
 
+
+def knapsack_algo(data, max_invested):
+    """Knapsack algorithm, timed."""
+
+    # Generate the matrix M(0...n, 0...W)
+    profit_matrix = make_profit_matrix(data, max_invested)
+    # [print(line_num, profit_matrix[line_num]) for line_num in range(len(profit_matrix))]
+
+    # Browse the matrix from maximum gain to 0,0
+    results = recursive_knap(profit_matrix, data, len(data)-1, max_invested)
+
+    return results
+
+
 def make_profit_matrix(data, money_thresholds):
-    min_price = money_thresholds[0]
-    max_spent = money_thresholds[1]
+    """Build and fill a profit matrix for knapsack algorithm resolution."""
+    min_price = min(data, key= lambda x:x["price"])["price"]
+    max_spent = money_thresholds
+
     data.insert(0, {"name":"action_0", "price":0, "gain":0})
     profit_matrix = []
 
     for action_index in range(len(data)):
         profit_matrix.append([0]*int(max_spent+1))
         # if data_index%100 == 0:
-        #       # To track huge datasets
+        #       # To keep track of huge datasets
         #     print(f"Matrix filed for action number {data_index}")
-        for remaining_money in range(min_price-1,max_spent+1):
+        for remaining_money in range(0,max_spent+1):
             price = data[action_index]["price"]
-            # print(f"Comparing {remaining_money} and {price}")
             if remaining_money < price:
                 # If we don't have no money, we can't buy
                 profit_matrix[action_index][remaining_money] = profit_matrix[action_index-1][remaining_money]
-                # print(profit_matrix[action_index-1][remaining_money])
-
-                # if we can't buy this one, we won't be able to buy the following
-                # so we can copy the whole remaining line
             else:
                 # If we do have the money, we have a choice to make. What is more profitable between us buying and not buying this action ?  
                 option_bought = profit_matrix[action_index-1][remaining_money-price] + data[action_index]["gain"]
@@ -55,21 +65,17 @@ def make_profit_matrix(data, money_thresholds):
 
     return profit_matrix
 
-# /**
-# * Returns the indices of the items of the optimal knapsack.
-# * i: We can include items 1 through i in the knapsack
-# * j: maximum weight of the knapsack
-# */
+
 def recursive_knap(profit_matrix, data, action_index, remaining_money, list_of_bought_actions = []):
     """Browse profit matrix from maximum gain to first action and/or 0 remaining money."""
     # print(profit_matrix[action_index][remaining_money])
-    # print(f"Checking {action_index},{remaining_money} (having already bought {list_of_bought_actions})")
+    # print(f"Checking {action_index},{remaining_money} ({profit_matrix[action_index][remaining_money]}) (having already bought {list_of_bought_actions})")
 
     if action_index == 0:
         return list_of_bought_actions
     
     if action_index not in list_of_bought_actions:
-        # print(profit_matrix[action_index][remaining_money])
+        # print(f"{profit_matrix[action_index][remaining_money]} > {profit_matrix[action_index-1][remaining_money]} ?")
         if profit_matrix[action_index][remaining_money] > profit_matrix[action_index-1][remaining_money]:
             price = data[action_index]['price']
             list_of_bought_actions.append(action_index)
@@ -77,65 +83,75 @@ def recursive_knap(profit_matrix, data, action_index, remaining_money, list_of_b
         else:
             # not bought
             return recursive_knap(profit_matrix, data, action_index-1, remaining_money, list_of_bought_actions)
+   
 
-def print_results(data, actions_list, sep="\t"):
+def greedy_algo(data, max_money):    
+    data = sorted(data, key= lambda action:action["gain"]/action["price"], reverse=True)
+    list_of_bought_actions = []
+    remaining_money = max_money
+    total_gain = 0
+    for action in data:
+        if remaining_money > action["price"]:
+            list_of_bought_actions.append(action)
+            remaining_money -= action["price"]
+            total_gain += action["gain"]
+
+    return list_of_bought_actions
+
+
+def load_fake_data():
+    """Fake dataset for demonstration and testing purposes"""
+    return [
+    {"name":"FA1", "price":3, "profit": 0.66, "gain": 2},
+    {"name":"FA2", "price":5, "profit": 1, "gain": 5},
+    {"name":"FA3", "price":8, "profit": 3, "gain": 24},
+    {"name":"FA4", "price":2, "profit": .5, "gain": 1},
+    {"name":"FA5", "price":1, "profit": 2, "gain": 2},
+    ]
+
+
+def print_results(actions_list, runtime, sep="\t"):
     """Show results Sienna's style"""
-    optimized_achats = actions_list
+    selected_actions = actions_list
     total_cost = 0
-    optimized_profit = 0
-    print("Bought:")
-    for action_index in optimized_achats:
-        action = data[action_index]
+    total_gain = 0
+
+
+    print(f"Results generated in {runtime}. Bought:")
+    for action in selected_actions:
         price_in_eur = action["price"]/100
-        gain = action["gain"]/100
-        ror = action["ror"]
-        print(f"{action['name']}{sep}{price_in_eur}{sep}{action['profit']}{sep}{gain}{sep}{ror}")
         total_cost += price_in_eur
-        optimized_profit += gain
+        total_gain += action["gain"]/100
+        # print(f"{action['name']}{sep}{price_in_eur}{sep}{action['profit']}{sep}{gain}{sep}{ror}")
+        print(f"{action['name']}{sep}{price_in_eur}")
 
     print(
         f"\nTotal cost: {round(total_cost,2)}\n",
-        f"Profit: {round(optimized_profit,2)}")
+        f"Profit: {round(total_gain,2)}")
+
 
 def main():
-    MAX_SPENT = 50000 # in cents
+    ALGO = "greedy"
+    MAX_SPENT = 50000 # in cents (10 is a good pick for fake data tests)
 
-    fake_data = [
-    {"name":"action1", "price":3, "profit": 0.1, "gain": 2},
-    {"name":"action2", "price":5, "profit": 0.1, "gain": 5},
-    {"name":"action3", "price":8, "profit": 0.1, "gain": 33},
-    {"name":"action4", "price":2, "profit": 0.1, "gain": 27},
-    {"name":"action4", "price":1, "profit": 0.1, "gain": 1},
-    ]
+    # Loading dataset
+    file_path = "./test_datasets/dataset1_Python+P7.csv"
+    data = load_csv(file_path) # or load_fake_data()
+    data = [action for action in data if action["price"] <= MAX_SPENT]
+    # [print(action) for action in data]
+    if data == []:
+        raise Exception("Dataset shouldn't be empty.")
 
-    # data = fake_data
-    file_path = "./test_datasets/test_dataset.csv"
-    # file_path = "./test_datasets/dataset1_Python+P7.csv"
-    data = load_csv(file_path, MAX_SPENT)
-    data = [action for action in data if action["price"] > 0 and action["price"] <= MAX_SPENT]
-    if data != []:
-        start = datetime.datetime.now()
-        min_price = min(data, key= lambda x:x["price"])["price"]
+    # Calling algorithms
+    start_time = datetime.datetime.now()
+    if ALGO == "knapsack":
+        # 0:1 Knapsack problem
+        results = knapsack_algo(data, MAX_SPENT)
+        results = [data[index] for index in results]
+    elif ALGO == "greedy":
+        results = greedy_algo(data, MAX_SPENT)
 
-        # data = data[:10]
-        # [print(action) for action in data]
-
-
-        data = sorted(data, key= lambda x:x["ror"], reverse=True)
-        # [print(action) for action in data[:10]]
-        # money_post_greed = MAX_SPENT - greedily_bought["price"]
-
-        # Generate the matrix M(0...n, 0...W)
-        profit_matrix = make_profit_matrix(data, [min_price, MAX_SPENT])
-        print(f"{datetime.datetime.now() - start}")
-        # [print(line_num, profit_matrix[line_num]) for line_num in range(len(profit_matrix))]
-
-        # From maximum gain, recursively browse the matrix towards 0,0
-        results = recursive_knap(profit_matrix, data, len(data)-1, MAX_SPENT)
-        print(f"{datetime.datetime.now() - start}")
-        print_results(data, results)        
-    else:
-        print(f"Input is empty.")
+    print_results(results, datetime.datetime.now() - start_time)
 
 if __name__ == '__main__':
     main()
